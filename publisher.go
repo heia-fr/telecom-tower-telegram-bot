3f052e3e59cc6404372bd80903d798ed56e957eb
Jacques Supcik <jacques.supcik@hefr.ch>
2016-01-15 16:05:34 +0100

    Simplify and remove stop-on-fail

--- publisher.go
+++ publisher.go
@@ -23,50 +23,32 @@ package main
 import (
 	"fmt"
 	"github.com/nats-io/nats"
-	"log"
 )
 
 type Line struct {
-	Text  string `json:"text"`
-	Font  int    `json:"font"`
-	Color string `json:"color"`
+	Text  string
+	Font  int
+	Color string
 }
 
 type RollingMessage struct {
-	Body         []Line `json:"body"`
-	Introduction []Line `json:"introduction"`
-	Conclusion   []Line `json:"conclusion"`
-	Separator    []Line `json:"separator"`
+	Body         []Line
+	Introduction []Line
+	Conclusion   []Line
+	Separator    []Line
 }
 
 var natsConn *nats.EncodedConn
 var natsSubject string
 
 func initPublisher(url string, subject string) {
-	nc, err := nats.Connect(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	natsConn, err = nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	if err != nil {
-		log.Fatal(err)
-	}
+	nc, _ := nats.Connect(url)
+	natsConn, _ = nats.NewEncodedConn(nc, nats.JSON_ENCODER)
 	natsSubject = subject
 }
 
 func (s *session) publishMessage(text string) {
 	towerMessage := RollingMessage{
-		Body: []Line{
-			Line{
-				Text:  fmt.Sprintf("%s says: ", s.sender.FirstName),
-				Font:  6,
-				Color: "#FFFFFF"},
-			Line{
-				Text:  text,
-				Font:  6,
-				Color: s.color},
-		},
 		Introduction: []Line{Line{Text: "", Font: 6, Color: "#000000"}},
 		Conclusion:   []Line{Line{Text: " // ", Font: 6, Color: "#0000FF"}},
 		Separator:    []Line{Line{Text: "  --  ", Font: 6, Color: "#FFFFFF"}},
